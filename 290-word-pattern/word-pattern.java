5d55dab9b18c0ac76185c11aceb8667d1518ea87
NishikantDounekar <119045534+nishikant23@users.noreply.github.com>
2024-06-11 23:48:41 +0530

    Time: 5 ms (5.97%) | Memory: 41.5 MB (23.29%) - LeetSync

--- 290-word-pattern/word-pattern.java
+++ 290-word-pattern/word-pattern.java
@@ -1,19 +1,26 @@
 class Solution {
     public boolean wordPattern(String s, String temp) {
         HashMap<Character, String> mapS = new HashMap<>();
+        HashMap<String, Character> mapT = new HashMap<>();
         String[] t = temp.split(" ");
-        if(s.length() != t.length) return false;
-        //for(String ch : t) System.out.println("Str: "+ch);
+        for(String ch : t) System.out.println("Str: "+ch);
 
-        for(int i=0; i<s.length(); i++){
+        if(s.length() != t.length) return false;
+
+        for(int i=0; i<t.length; i++){
             char chs = s.charAt(i);
             String cht = t[i];
-            if(mapS.containsKey(chs)){ 
-                if(!mapS.get(chs).equals(cht)) return false;
-            }else {
-                if(mapS.containsValue(cht)) return false;
-
+            if(!mapS.containsKey(chs)){ 
                 mapS.put(chs, cht);
+            }else {
+                if(!mapS.get(chs).equals(cht)) return false;
+                //else continue;
+            }
+            if(!mapT.containsKey(cht)){ 
+                mapT.put(cht, chs);
+            }else {
+                if(!mapT.get(cht).equals(chs)) return false;
+                //else continue;
             }
         }
         return true;
