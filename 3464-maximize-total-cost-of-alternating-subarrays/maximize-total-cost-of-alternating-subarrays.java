20b6213261770138e20a72f0f7b756ab4f1141cd
NishikantDounekar <119045534+nishikant23@users.noreply.github.com>
2024-06-24 22:41:38 +0530

    Time: 2 ms (100.00%) | Memory: 59.1 MB (100.00%) - LeetSync

--- 3464-maximize-total-cost-of-alternating-subarrays/maximize-total-cost-of-alternating-subarrays.java
+++ 3464-maximize-total-cost-of-alternating-subarrays/maximize-total-cost-of-alternating-subarrays.java
@@ -2,13 +2,13 @@ class Solution {
     public long maximumTotalCost(int[] nums) {
 
        int n = nums.length;
-       long[] dp = new long[n+1];
+       long[] dp = new long[n+1]; //dp arr store each sub-arr sums
        dp[1]  = (long) nums[0];
         for(int i=1; i<n; i++ ){
-            long op1 = dp[i] + nums[i];
-            long op2 = dp[i-1] + nums[i-1] + (-1)*nums[i];
+            long op1 = dp[i] + nums[i]; //if +ve curr = n[i], op1 is max
+            long op2 = dp[i-1] + nums[i-1] + (-1)*nums[i]; //-ve=n[i] op2 is max
             dp[i+1] = Math.max(op1, op2);
         }
-        return dp[n];
+        return dp[n]; //dp[last] stores the sub-arr whose sum is max;
     }
 }
