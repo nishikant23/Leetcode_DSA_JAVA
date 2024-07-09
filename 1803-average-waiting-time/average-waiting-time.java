1bd73c7fab3af336eaebbb4c33661b783f9e8994
NishikantDounekar <119045534+nishikant23@users.noreply.github.com>
2024-07-09 18:04:30 +0530

    Time: 501 ms (5.31%) | Memory: 96.7 MB (5.31%) - LeetSync

---
+++ 1803-average-waiting-time/average-waiting-time.java
@@ -0,0 +1,26 @@
+class Solution {
+    public double averageWaitingTime(int[][] cust) {
+        double timeTaken=0;
+        double waitTime=0;
+
+        for(int i=0; i<cust.length; i++) {
+            if(timeTaken==0){
+                timeTaken = cust[i][0] + cust[i][1];
+                waitTime += Math.abs(timeTaken - cust[i][0]);
+                System.out.println("Time taken = "+timeTaken+" & Wait time = "+waitTime+" Arrival Time = "+cust[i][0]);
+            }else{
+                if(timeTaken>= cust[i][0]){
+                    timeTaken += cust[i][1];
+                    waitTime += Math.abs(timeTaken - cust[i][0]);
+                    System.out.println("Time taken = "+timeTaken+" & Wait time = "+waitTime+" Arrival Time = "+cust[i][0]);
+                }else{
+                    timeTaken = cust[i][0] + cust[i][1];
+                    waitTime += Math.abs(timeTaken - cust[i][0]);
+                    System.out.println("Time taken = "+timeTaken+" & Wait time = "+waitTime+" Arrival Time = "+cust[i][0]);
+                }
+                
+            }
+        }
+        return waitTime/cust.length;
+    }
+}
