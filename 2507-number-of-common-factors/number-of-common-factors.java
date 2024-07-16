6e4be080fa78e1322f05f37abd9759313f1cee40
NishikantDounekar <119045534+nishikant23@users.noreply.github.com>
2024-07-16 22:32:48 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (73.70%) - LeetSync

---
+++ 2507-number-of-common-factors/number-of-common-factors.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int commonFactors(int a, int b) {
+       int gcdValue = gcd(a, b);
+        return countFactors(gcdValue);
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    private int countFactors(int num) {
+        int count = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
