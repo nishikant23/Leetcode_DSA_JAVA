d197ec3c3e68af8d3a96531d7474f4c27e0ac6da
NishikantDounekar <119045534+nishikant23@users.noreply.github.com>
2024-07-23 15:29:03 +0530

    Time: 7 ms (55.67%) | Memory: 44.5 MB (41.14%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
@@ -16,43 +16,17 @@ class Solution {
         for(int i=0; i<nums.length; i++){
             pair[i] = new Pair(nums[i], map.get(nums[i]));
         }
-        Arrays.sort(pair, (a,b) -> {
+        Arrays.sort(pair, (a,b) -> { //Comparator Chaining
             if(a.freq != b.freq)
                return a.freq - b.freq;
             else 
                return b.num - a.num;
         });
-        //Debugging
-        // for(int i=0; i<nums.length; i++){
-        //     System.out.println("Num = "+pair[i].num+" Frequency = "+pair[i].freq);
-        // }
+
         int[] sortedNums = new int[nums.length];
         for(int i=0; i<nums.length; i++){
             sortedNums[i] = pair[i].num;
         }
-        // int l=0, r=0;
-        // for(int i=0; i<nums.length; i++){
-        //     l=i;
-        //     for(int j=i; j<pair.length; j++) {
-        //         if(pair[i].freq != pair[j].freq){ 
-                    
-        //             r=j-1;
-        //             i = j;
-        //             break;
-        //         }
-        //     }
-        //     sort(pair, l, r);
-        // }
         return sortedNums;
-    }
-    public void sort(Pair[] pair, int l, int r){
-        int[] arr = new int[r-l+1];
-        for(int i=l; i<=r; i++){
-            arr[i] = pair[i].num;
-        }
-        Arrays.sort(arr);
-        for(int i=0; i<arr.length; i++){
-            pair[i].num = arr[i];
-        }
     }
 }
