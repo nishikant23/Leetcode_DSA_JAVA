16f1006f9059c53ae0c3aad22a16938274afd86e
NishikantDounekar <119045534+nishikant23@users.noreply.github.com>
2024-07-31 18:08:40 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (75.95%) - LeetSync

---
+++ 2715-k-items-with-the-maximum-sum/k-items-with-the-maximum-sum.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+        if(k==0){
+            return 0;
+        }
+
+        int sum=0;
+
+        if(k!=0){
+                if(k<=numOnes){
+                    sum+=k;
+                    k=0;
+                }
+                else{
+                    k-=numOnes;
+                    sum+=numOnes;
+                    numOnes=0;
+                }
+            }
+            if(k!=0){
+                if(k<=numZeros){
+                    k=0;
+                }
+                else{
+                    k-=numZeros;
+                    numZeros=0;
+                }
+
+            }
+            if(k!=0){
+               if(k<=numNegOnes){
+                    sum-=k;
+                    k=0;
+                }
+                else{
+                    k-=numNegOnes;
+                    sum-=numNegOnes;
+                    numNegOnes=0;
+                }
+            }
+
+        return sum;
+    }
+}
